2e4f7e3b7c469decd567c0c405cdbcd1cdcde49a
Alexander Alexandrov <alexander@alexandroff.me>
2019-12-03 13:55:05 +0200

    First Commit

--- 1-navigate-between-pages/pages/index.js
+++ 1-navigate-between-pages/pages/index.js
@@ -1,7 +1,13 @@
-export default function Index() {
-  return (
-    <div>
-      <p>Hello Next.js</p>
-    </div>
-  )
-}
+// This is the Link API
+import Link from 'next/link';
+
+const Index = () => (
+  <div>
+    <Link href="/about">
+      <a>About Page</a>
+    </Link>
+    <p>Hello Next.js</p>
+  </div>
+);
+
+export default Index;
